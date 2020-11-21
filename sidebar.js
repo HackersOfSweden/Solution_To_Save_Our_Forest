98e6456662cc953cb54ec53423e4ed7efe90c10d
lharesign <luke_haresign@hotmail.co.uk>
2020-11-21 20:24:23 +0100

    created index, kata, details

--- sidebar.js
+++ sidebar.js
@@ -1,36 +1,15 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function navEvent() {
-    if ($("#mySidebar").hasClass("closed")) {
-        
-        $("#mySidebar").css("width", "250px");
-        $("#mySidebar").removeClass("closed");
-        $("#mySidebar").addClass("open");
-        $(".openbtn").css("visibility", "hidden");
-
-    } else if ($("#mySidebar").hasClass("open")) {
-        
-        $("#mySidebar").css("width", "0px");
-        $("#mySidebar").removeClass("open");
-        $("#mySidebar").addClass("closed");
-        $(".openbtn").css("visibility", "visible");
+function login() {
+    if ($(".input-element").val() != "") {
+        window.location.href = "details.html";
+    } else {
+        alert("Skriv in ditt personnummer för att fortsätta");
     }
 }
 
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+function saveDetails() {
+    if ($(".email-input").val() != "" && $(".mobile-input").val() != "") {
+        window.location.href = "karta.html";
     } else {
-      panel.style.display = "block";
+        alert("Skriv in din epostaddress och mobilnummer för att fortsätta");
     }
-  });
 }
